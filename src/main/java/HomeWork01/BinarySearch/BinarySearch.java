a984ea6bcdcc41fa8f075452dafe3abe120c6020
Nikulchenko Nick <nikulchenko.nn@gmail.com>
2020-12-09 16:56:10 +0300

    With all changes

---
+++ src/main/java/HomeWork01/BinarySearch/BinarySearch.java
@@ -0,0 +1,33 @@
+package HomeWork01.BinarySearch;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] list = new int[10];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = i;
+        }
+        System.out.println(binarySearch(list, 10));
+    }
+
+    public static boolean binarySearch(int[] list, int n) {
+        Arrays.sort(list);
+        int start = 0;
+        int end = list.length - 1;
+        while (start <= end) {
+            int middle = (start + end) / 2;
+            if (list[middle] == n) {
+                return true; }
+            else if (list[middle] < n) {
+                start = middle + 1;
+            }
+            else if (list[middle] > n) {
+                end = middle - 1;
+            }
+        }
+        return false;
+    }
+}
--- src/main/java/HomeWork01/Converter/Converter.java
+++ src/main/java/HomeWork01/Converter/Converter.java
@@ -3,14 +3,15 @@ package HomeWork01.Converter;
 public class Converter {
     public static void main(String[] args) {
         System.out.println(converter(36.6));
-        System.out.println(decoverter(45.75));
+        System.out.println(deconverter(45.75));
     }
 
     public static double converter(double t) {
         return t*1.25;
     }
 
-    public static double decoverter(double t) {
+    public static double deconverter(double t) {
         return t/1.25;
     }
 }
+
