b98293554e8f3cc2e8aa451b9aaa17e16452c818
NikulchenkoNN <nikulchenko.nn@gmail.com>
2020-12-17 01:45:24 +0300

    new

---
+++ src/main/java/HomeWork02/Task02/ReadFromFile.java
@@ -0,0 +1,21 @@
+package HomeWork02.Task02;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadFromFile {
+    protected static List<Word> reader(String fileName) throws IOException {
+        List<Word> words = new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        while (bufferedReader.ready()) {
+            String[] line = bufferedReader.readLine().split(" ");
+            for (String str : line) {
+                words.add(new Word(str));
+            }
+        }
+        return words;
+    }
+}
--- src/main/java/HomeWork02/Task02/Task02Main.java
+++ src/main/java/HomeWork02/Task02/Task02Main.java
@@ -1,26 +1,17 @@
 package HomeWork02.Task02;
 
-
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.List;
 
 public class Task02Main {
-    public static void main(String[] args) {
-        List<Word> words = new ArrayList<>();
-        words.add(new Word("mama"));
-        words.add(new Word("mila"));
-        words.add(new Word("milom"));
-        words.add(new Word("ramu"));
-        words.add(new Word("mil"));
-        words.add(new Word("milloin"));
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        List<Word> words = ReadFromFile.reader(bufferedReader.readLine());
         System.out.println(words);
-        System.out.println();
         Collections.sort(words);
         System.out.println(words);
     }
-
-
-    
 }
